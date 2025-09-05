3e63680ba2015b125d081a097a33450cfbd3fad7
hung123 <nguyencuingvlhl@gmail.com>
2025-09-05 23:13:55 +0700

    ok

---
+++ src/routes/index.js
@@ -0,0 +1,35 @@
+import Home from '@pages/Home';
+import Following from '@pages/Following';
+import Profile from '@pages/Profile';
+import Upload from '@pages/Upload';
+import Search from '@pages/Search';
+import { HeaderOnly } from '../components/Layout';
+
+const publicRoutes = [
+    {
+        path: '/',
+        conponent: Home,
+    },
+    {
+        path: '/following',
+        conponent: Following,
+    },
+    {
+        path: '/profile',
+        conponent: Profile,
+    },
+    {
+        path: '/upload',
+        conponent: Upload,
+        layout: HeaderOnly
+    },
+    {
+        path: '/search',
+        conponent: Search,
+        layout: null
+    },
+];
+
+const privateRoutes = [];
+
+export { privateRoutes, publicRoutes };
